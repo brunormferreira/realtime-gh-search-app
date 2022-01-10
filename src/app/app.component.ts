f732d6074e7627fd65702fc828b946014fd8b03e
Bruno Ramires <brunormferreira@hotmail.com>
2022-01-10 12:19:14 -0300

    add(feat): search engine with rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Observable,
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+} from 'rxjs';
+import { SearchService } from './services/search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'realtime-gh-search';
+export class AppComponent implements OnInit {
+  queries$ = new Subject<string>();
+  repositories$!: Observable<any[]>;
+
+  page: number = 1;
+  paginationEl: any[] = [];
+
+  constructor(private searchService: SearchService) {}
+
+  ngOnInit() {
+    this.repositories$ = this.queries$.pipe(
+      map((query: string) => (query ? query.trim() : '')),
+      filter(Boolean),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((query: string) => this.searchService.fetchRepositories(query))
+    );
+
+    this.setPagination();
+  }
+
+  searchRepos($event: any) {
+    this.queries$.next($event.target.value);
+  }
+
+  setPagination(): void {
+    this.repositories$.subscribe((repositories: any[]) => {
+      this.paginationEl = repositories;
+    });
+  }
 }
