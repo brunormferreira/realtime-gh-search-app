6ef51ad46fbf6743f07eb5a21109b34b1e90ba43
Bruno Ramires <brunormferreira@hotmail.com>
2022-01-10 17:38:41 -0300

    add(feat): att modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NgProgressModule } from 'ngx-progressbar';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { ProgressInterceptor } from './services/progress.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +19,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     NgProgressModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ProgressInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
